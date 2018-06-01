755f8a540d5d598a3d06fc91daed6fabeae2e39e
Jelle Overbeek <j.overbeek@recognize.nl>
2018-06-01 10:37:00 +0200

    added folder structure

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -5,46 +5,53 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const gulpSequence = require('gulp-sequence');
 const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
+
+const source = './src/'
+const destination = './static/'
 
 gulp.task('sass', function () {
-    return gulp.src('./assets/scss/**/*.scss')
+    return gulp.src(source + 'scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./build/assets/css'));
+        .pipe(gulp.dest(destination + 'css'));
 });
 
 gulp.task('img', function () {
-    return gulp.src('./assets/img/**/*')
-        .pipe(gulp.dest('./build/assets/img'))
+    return gulp.src(source + 'img/**/*')
+        .pipe(gulp.dest(destination + 'img'))
 });
 
 gulp.task('img:compress', function () {
-    return gulp.src('./assets/img/**/*')
+    return gulp.src(source + 'img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./build/assets/img'))
+        .pipe(gulp.dest(destination + 'img'))
 });
 
 gulp.task('js', function () {
-    return gulp.src('./assets/js/**/*.js')
-        .pipe(gulp.dest('./build/assets/js'))
+    return gulp.src(source + 'js/**/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest(destination + 'js'))
 });
 
 gulp.task('js:minify', () => {
-    return gulp.src('./build/assets/js/**/*.js')
+    return gulp.src(destination + '/js/**/*.js')
         .pipe(babel({
             presets: ['env']
         }))
         .pipe(uglify())
-        .pipe(gulp.dest('./build/assets/js'))
+        .pipe(gulp.dest(destination + 'js'))
 });
 
 gulp.task('css:minify',() => {
-    return gulp.src('./build/assets/css/**/*.css')
+    return gulp.src(destination + 'css/**/*.css')
         .pipe(cleanCSS())
-        .pipe(gulp.dest('./build/assets/css'));
+        .pipe(gulp.dest(destination + 'css'));
 });
 
 gulp.task('clean', function () {
-    return gulp.src('./build', {read: false})
+    return gulp.src(destination, {read: false})
         .pipe(clean());
 });
 
@@ -52,5 +59,5 @@ gulp.task('watch', function () {
     gulp.watch('./assets/**/*', ['default']);
 });
 
-gulp.task('default', ['sass', 'js', 'img', 'twig']);
+gulp.task('default', ['sass', 'js', 'img']);
 gulp.task('build',  gulpSequence('clean', ['sass', 'js', 'img'], ['js:minify', 'css:minify', 'img:compress']));
