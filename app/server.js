0934abe240c536b6ba971b513592e801d024890b
Jelle Overbeek <j.overbeek@recognize.nl>
2018-06-05 14:01:10 +0200

    added more content to timeline

--- app/server.js
+++ app/server.js
@@ -12,14 +12,14 @@ const programData = {
         label: null,
         sticky: true,
         items: [{
-            name: "CSS to the rescue",
+            name: "Web App From Scratch",
             iconSrc: "//via.placeholder.com/64x64",
             url: "#",
             description: "Lorem ipsum dolor",
             partner_id: null,
             type: "course"
         }, {
-            name: "Web app from scratch",
+            name: "CSS To The Rescue",
             iconSrc: "//via.placeholder.com/64x64",
             url: "#",
             description: "Lorem ipsum dolor",
@@ -39,13 +39,13 @@ const programData = {
         items: [
             {
                 type: "assignment",
-                description: "Herhalen van Node/npm basics; Opzetten van je ontwikkel omgeving;"
+                description: "Het structureren van JavaScript heeft een aantal voordelen. Het maakt de code beter leesbaar en dus makkelijker onderhoudbaar en uitbreidbaar."
             }, {
                 type: "assignment",
-                description: "Bouwen van een basic app en deployen."
+                description: "De student kan een goed werkende responsive oplossing bedenken en maken voor complexe interfaces"
             }, {
                 type: "speaker",
-                name: "Justus Sturkenboom",
+                name: "Titus Wormer",
                 url: "#",
                 avatarURL: "//via.placeholder.com/64x64"
             }
@@ -62,7 +62,7 @@ const programData = {
                 description: "Bouwen van een basic app en deployen."
             }, {
                 type: "speaker",
-                name: "Justus Sturkenboom",
+                name: "PPK",
                 url: "#",
                 avatarURL: "//via.placeholder.com/64x64"
             }
@@ -91,6 +91,180 @@ const programData = {
             {
                 type: "project",
                 name: "Project 1",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur.",
+                url: "#"
+            }
+        ]
+    }, {
+        label: null,
+        sticky: true,
+        items: [{
+            name: "Browser Technologies",
+            iconSrc: "//via.placeholder.com/64x64",
+            url: "#",
+            description: "Lorem ipsum dolor",
+            partner_id: null,
+            type: "course"
+        }, {
+            name: "Performance Matters",
+            iconSrc: "//via.placeholder.com/64x64",
+            url: "#",
+            description: "Lorem ipsum dolor",
+            partner_id: null,
+            type: "course"
+        }, {
+            name: "Weekly Nerd",
+            iconSrc: "//via.placeholder.com/64x64",
+            url: "#",
+            description: "Lorem ipsum dolor",
+            partner_id: null,
+            type: "course"
+        }]
+    }, {
+        label: "Week 5",
+        sticky: false,
+        items: [
+            {
+                type: "assignment",
+                description: "Het structureren van JavaScript heeft een aantal voordelen. Het maakt de code beter leesbaar en dus makkelijker onderhoudbaar en uitbreidbaar."
+            }, {
+                type: "assignment",
+                description: "De student kan een goed werkende responsive oplossing bedenken en maken voor complexe interfaces"
+            }, {
+                type: "speaker",
+                name: "Anne - TamTam",
+                url: "#",
+                avatarURL: "//via.placeholder.com/64x64"
+            }
+        ]
+    }, {
+        label: "Week 6",
+        sticky: false,
+        items: [
+            {
+                type: "assignment",
+                description: "Herhalen van Node/npm basics; Opzetten van je ontwikkel omgeving;"
+            }, {
+                type: "assignment",
+                description: "Bouwen van een basic app en deployen."
+            }, {
+                type: "speaker",
+                name: "PE - Voorhoede",
+                url: "#",
+                avatarURL: "//via.placeholder.com/64x64"
+            }
+        ]
+    }, {
+        label: "Week 7",
+        sticky: false,
+        items: [
+            {
+                type: "assignment",
+                description: "Herhalen van Node/npm basics; Opzetten van je ontwikkel omgeving;"
+            }, {
+                type: "assignment",
+                description: "Bouwen van een basic app en deployen."
+            }, {
+                type: "speaker",
+                name: "Ischa Gast - Accessibility",
+                url: "#",
+                avatarURL: "//via.placeholder.com/64x64"
+            }
+        ]
+    }, {
+        label: "Week 8",
+        sticky: false,
+        items: [
+            {
+                type: "project",
+                name: "Project 2",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur.",
+                url: "#"
+            }
+        ]
+    }, {
+        label: null,
+        sticky: true,
+        items: [{
+            name: "Web Design",
+            iconSrc: "//via.placeholder.com/64x64",
+            url: "#",
+            description: "Lorem ipsum dolor",
+            partner_id: null,
+            type: "course"
+        }, {
+            name: "Real Time Web",
+            iconSrc: "//via.placeholder.com/64x64",
+            url: "#",
+            description: "Lorem ipsum dolor",
+            partner_id: null,
+            type: "course"
+        }, {
+            name: "Weekly Nerd",
+            iconSrc: "//via.placeholder.com/64x64",
+            url: "#",
+            description: "Lorem ipsum dolor",
+            partner_id: null,
+            type: "course"
+        }]
+    }, {
+        label: "Week 9",
+        sticky: false,
+        items: [
+            {
+                type: "assignment",
+                description: "Het structureren van JavaScript heeft een aantal voordelen. Het maakt de code beter leesbaar en dus makkelijker onderhoudbaar en uitbreidbaar."
+            }, {
+                type: "assignment",
+                description: "De student kan een goed werkende responsive oplossing bedenken en maken voor complexe interfaces"
+            }, {
+                type: "speaker",
+                name: "Niels Leenheer",
+                url: "#",
+                avatarURL: "//via.placeholder.com/64x64"
+            }
+        ]
+    }, {
+        label: "Week 10",
+        sticky: false,
+        items: [
+            {
+                type: "assignment",
+                description: "Herhalen van Node/npm basics; Opzetten van je ontwikkel omgeving;"
+            }, {
+                type: "assignment",
+                description: "Bouwen van een basic app en deployen."
+            }, {
+                type: "speaker",
+                name: "Peter Peerdeman",
+                url: "#",
+                avatarURL: "//via.placeholder.com/64x64"
+            }
+        ]
+    }, {
+        label: "Week 11",
+        sticky: false,
+        items: [
+            {
+                type: "assignment",
+                description: "Herhalen van Node/npm basics; Opzetten van je ontwikkel omgeving;"
+            }, {
+                type: "assignment",
+                description: "Bouwen van een basic app en deployen."
+            }, {
+                type: "speaker",
+                name: "Leonie Watson - W3C",
+                url: "#",
+                avatarURL: "//via.placeholder.com/64x64"
+            }
+        ]
+    }, {
+        label: "Week 12",
+        sticky: false,
+        items: [
+            {
+                type: "project",
+                name: "Project 3",
                 description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur.",
                 url: "#"
             }
