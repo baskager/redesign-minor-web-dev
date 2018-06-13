ed1683394ef4936718a9048cd1a26a7dc52e8935
Rick712 <rick.buter@gmail.com>
2018-06-13 15:20:45 +0200

    added awesome animation

--- app/src/js/animation.js
+++ app/src/js/animation.js
@@ -1,9 +1,34 @@
-const   signUpForm = document.querySelector('.sign-up-form'),
-        signUpBtn = signUpForm.querySelector('input[type="submit"]')
+(function(){
+    const   signUpForm = document.querySelector('.sign-up-form'),
+            coursesProgram = document.querySelectorAll('.course-cssttr')
 
-    signUpBtn.addEventListener('click', function() {
-        // if (signUpForm.checkValidity()) {
-            if (true) {
-            signUpForm.classList.add('form-submit')
-        } 
-    })
+    if (signUpForm) {
+        const signUpBtn = signUpForm.querySelector('input[type="submit"]')
+        
+        signUpBtn.addEventListener('click', function() {
+            // if (signUpForm.checkValidity()) {
+                if (true) {
+                signUpForm.classList.add('form-submit')
+            } 
+        })
+    }
+
+    if(coursesProgram) {
+        coursesProgram.forEach(course => {
+            course.addEventListener('click', function(event) {
+                
+                const stopHref = function() {
+                    event.preventDefault()
+                    course.classList.add('flip-2-hor-top-fwd')
+                },
+
+                linkPage = function() {
+                    const href = course.querySelector('a').href
+                    window.location.href = href
+                }
+                stopHref()
+                window.setTimeout(linkPage, 1000)
+            })
+        })
+    }
+})()
