9c642c2bcba4b36e36c4627c562619a931578bc3
Rick712 <rick.buter@gmail.com>
2018-06-13 12:39:52 +0200

    changed button pulse animation a bit and structured scss file

--- app/src/js/animation.js
+++ app/src/js/animation.js
@@ -2,7 +2,8 @@ const   signUpForm = document.querySelector('.sign-up-form'),
         signUpBtn = signUpForm.querySelector('input[type="submit"]')
 
     signUpBtn.addEventListener('click', function() {
-        if (signUpForm.checkValidity()) {
+        // if (signUpForm.checkValidity()) {
+            if (true) {
             signUpForm.classList.add('form-submit')
         } 
     })
