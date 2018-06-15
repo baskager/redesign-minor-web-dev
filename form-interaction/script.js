b6b5bce558b61a0f4d6ccea850f59a3cdc94336e
Rick712 <rick.buter@gmail.com>
2018-06-15 15:51:06 +0200

    fixed something

--- form-interaction/script.js
+++ form-interaction/script.js
@@ -25,6 +25,84 @@ const emailDomain = {
     }
 }
 
+const letters = {
+  11: {
+    value: "a"
+  },
+  12: {
+    value: "b"
+  },
+  13: {
+    value: 'c'
+    },
+  14: {
+    value: 'd'
+    },
+  16: {
+    value: 'e'
+    },
+  21: {
+    value: 'f'
+    },
+  22: {
+    value: 'g'
+    },
+  23: {
+    value: 'h'
+    },
+  24: {
+    value: 'i'
+    },
+  25: {
+    value: 'j'
+    },
+  31: {
+    value: 'k'
+    },
+  32: {
+    value: 'l'
+    },
+  33: {
+    value: 'm'
+    },
+  34: {
+    value: 'n'
+    },
+  35: {
+    value: 'o'
+    },
+  41: {
+    value: 'p'
+    },
+  42: {
+    value: 'q'
+    },
+  43: {
+    value: 'r'
+    },
+  44: {
+    value: 's'
+    },
+  45: {
+    value: 't'
+    },
+  51: {
+    value: 'u'
+    },
+  52: {
+    value: 'v'
+    },
+  53: {
+    value: 'w'
+    },
+  54: {
+    value: 'x'
+    },
+  55: {
+    value: 'z'
+    },
+};
+
 const emailEnd = document.querySelector('.form2 #email-end')
 
 emailEnd.addEventListener('keyup', function() {
@@ -52,3 +130,15 @@ emailEnd.addEventListener('keyup', function() {
         }
     })
 })
+
+const focus = document.querySelectorAll('input')
+
+Object.keys(letters).forEach(letter => {
+    focus.forEach(el => {
+        el.addEventListener('keyup', function () {
+            if(this.value == letter) {
+                el.value = letters[letter].value
+            }
+        })
+    })
+});
