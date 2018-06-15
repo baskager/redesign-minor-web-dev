c9a8a57c001a21b6929e4b022f5934d70cad4be7
Jamal van Rooijen <jamal@vergelijkgroep.nl>
2018-06-15 16:12:49 +0200

    Functionality not working when input field is focussed

--- app/src/js/nav.js
+++ app/src/js/nav.js
@@ -1,51 +1,47 @@
 var keyNavSwitch = true;
 
-var keyNav = function() {
-  if (keyNavSwitch == true) {
-    document.addEventListener(
-      "keyup",
-      function(e) {
-        // 1
-        if (e.keyCode == 49) {
-          window.location.href = "/";
-        }
-        // 2
-        else if (e.keyCode == 50) {
-          window.location.href = "/program";
-        }
-        // 3
-        else if (e.keyCode == 51) {
-          window.location.href = "/partners";
-        }
-        // 4
-        else if (e.keyCode == 52) {
-          window.location.href = "/student-work";
-        }
-        // 5
-        else if (e.keyCode == 53) {
-          window.location.href = "/contact";
-        }
-        // 6
-        else if (e.keyCode == 54) {
-          window.location.href = "/signup";
-        }
-      },
-      false
-    );
+document.addEventListener("keyup", function(e) {
+  if (keyNavSwitch === true) {
+    keyNav(e);
+  }
+});
+
+var keyNav = function(e) {
+  // 1
+  if (e.keyCode === 49) {
+    window.location.href = "/";
+  }
+  // 2
+  else if (e.keyCode === 50) {
+    window.location.href = "/program";
+  }
+  // 3
+  else if (e.keyCode === 51) {
+    window.location.href = "/partners";
+  }
+  // 4
+  else if (e.keyCode === 52) {
+    window.location.href = "/student-work";
+  }
+  // 5
+  else if (e.keyCode === 53) {
+    window.location.href = "/contact";
+  }
+  // 6
+  else if (e.keyCode === 54) {
+    window.location.href = "/signup";
   }
 };
 
-var inputCheck = function() {
-  var inputText = document.querySelectorAll("input[type=text]");
-  // var inputArea = document.querySelectorAll("textarea");
-
-  inputText.forEach(el => {
-    el.addEventListener("focus", el => {
-      keyNavSwitch = false;
-    });
+var input = document.querySelectorAll("input");
+input.forEach(el => {
+  el.addEventListener("focus", el => {
+    keyNavSwitch = false;
   });
+});
 
-  keyNav();
-};
-
-inputCheck();
+input.forEach(el => {
+  el.addEventListener("focusout", el => {
+    keyNavSwitch = true;
+  });
+});
