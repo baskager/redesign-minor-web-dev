43eb5ac6bdbeceadbb113ce55e259bb66f4e3828
Jelle Overbeek <j.overbeek@recognize.nl>
2018-06-18 16:14:04 +0200

    created classes of objects

--- app/src/js/splitscreen-presentation.js
+++ app/src/js/splitscreen-presentation.js
@@ -1,4 +1,5 @@
 // (function() {
+
 const config = {
   intervalTime: 500
 };
@@ -42,213 +43,247 @@ const slides = [
   }
 ];
 
-const subtitles = {
-  interval: null,
-  subtitles: subs,
-  subtitleEl: document.querySelector(".subtitle"),
-  alignSubs: function(side) {
-    this.subtitleEl.classList.remove("right");
-    this.subtitleEl.classList.remove("left");
-    this.subtitleEl.classList.add(side);
-  },
-  getSubtitle: function(timestamp) {
-    let vidTime = presentation.video.currentTime * 1000;
+class Subtitles {
+  constructor(element, subtitles) {
+    this.interval = null;
+    this.subtitleEl = element;
+    this.subtitles = subtitles;
 
-    const currentSubtitles = this.subtitles.filter(function(sub) {
-      if (vidTime > sub.startTime && vidTime < sub.endTime) {
-        return sub;
-      }
-    });
+    this.alignSubs = function(side) {
+      this.subtitleEl.classList.remove("right");
+      this.subtitleEl.classList.remove("left");
+      this.subtitleEl.classList.add(side);
+    };
 
-    let currentSub = currentSubtitles[currentSubtitles.length - 1];
-    return currentSub;
-  },
-  setSubtitle: function() {
-    let currentSub = this.getSubtitle();
-    let text = "";
+    this.getSubtitle = function(timestamp) {
+      let vidTime = presentation.video.currentTime * 1000;
 
-    if (currentSub) {
-      text = currentSub.text;
-    }
-
-    this.subtitleEl.innerText = text;
-  },
-  checkCursorSide: function(cursorPos) {
-    let screenWidth = window.innerWidth;
-    let screenCenter = screenWidth / 2;
-    let side;
-
-    if (cursorPos > screenCenter) {
-      side = "right";
-    } else {
-      side = "left";
-    }
-
-    return side;
-  },
-  setCursorEvent: function() {
-    presentation.media.addEventListener("mousemove", ev => {
-      let side = this.checkCursorSide(ev.screenX);
-      this.alignSubs(side);
-    });
-  },
-  init: function() {
-    this.setCursorEvent();
-  }
-};
-
-const presentation = {
-  media: document.querySelector(".media"),
-  video: document.querySelector(".media video"),
-  slideEl: document.querySelector(".media .current-slide"),
-  slidesEl: document.querySelector(".slides"),
-  seekerEl: document.querySelector(".seeker"),
-  speedUpEl: document.querySelector(".speed-up"),
-  speedDownEl: document.querySelector(".speed-down"),
-  speedDropdown: document.querySelector("#set-speed"),
-  currentSlideEl: null,
-  playing: false,
-  slideNodes: [],
-  videoDuration: 0,
-  interval: null,
-  createSlides: function() {
-    slides.forEach(slide => {
-      let img = document.createElement("img");
-      img.src = slide.img;
-
-      img.addEventListener("click", ev => {
-        this.changeTime(slide.startTime);
-        this.updatePresentation();
+      const currentSubtitles = this.subtitles.filter(function(sub) {
+        if (vidTime > sub.startTime && vidTime < sub.endTime) {
+          return sub;
+        }
       });
 
-      this.slideNodes.push(img);
-      this.slidesEl.insertAdjacentElement("beforeend", img);
-    });
-  },
-  getSlide: function(currentTime) {
-    let currentSlide = null;
+      let currentSub = currentSubtitles[currentSubtitles.length - 1];
+      return currentSub;
+    };
 
-    slides.filter(function(slide) {
-      if (currentTime >= slide.startTime && currentTime <= slide.endTime) {
-        currentSlide = slide;
+    this.setSubtitle = function() {
+      let currentSub = this.getSubtitle();
+      let text = "";
+
+      if (currentSub) {
+        text = currentSub.text;
       }
-    });
 
-    return currentSlide;
-  },
-  setActiveSlide: function(slide) {
-    this.slideNodes.forEach(function(slide) {
-      slide.classList.remove("active");
-    });
+      this.subtitleEl.innerText = text;
+    };
 
-    this.currentSlideEl = this.slideNodes[slide.index];
-    this.currentSlideEl.classList.add("active");
-  },
-  setSlide: function() {
-    let slide = this.getSlide(this.video.currentTime);
-    if (slide) {
-      this.slideEl.src = slide.img;
-      this.setActiveSlide(slide);
-    }
-  },
-  slideChanged: function() {},
-  changeTime: function(time) {
-    this.video.currentTime = time;
-  },
-  setSeeker: function() {
-    this.video.addEventListener("durationchange", ev => {
-      this.duration = this.video.duration;
-      this.seekerEl.max = this.duration;
-    });
-  },
-  updateSeeker: function() {
-    this.seekerEl.value = this.video.currentTime;
-  },
-  handleSeeker: function() {
-    const _this = this;
-    this.seekerEl.addEventListener("change", function() {
-      _this.changeTime(this.value);
-      _this.updatePresentation();
-    });
-  },
-  startInterval: function() {
-    const _this = this;
-    this.interval = setInterval(function() {
-      _this.updatePresentation();
-    }, config.intervalTime);
-  },
-  updatePresentation: function() {
-    this.setSlide();
-    this.updateSeeker();
-    subtitles.setSubtitle();
+    this.checkCursorSide = function(cursorPos) {
+      let screenWidth = window.innerWidth;
+      let screenCenter = screenWidth / 2;
+      let side;
 
-    if (this.video.currentTime === this.duration) {
-      this.pause();
-    }
-  },
-  play: function() {
-    location.href = "#presentation";
-    this.video.play();
-    this.playing = true;
-    this.media.classList.remove("paused");
-    this.startInterval();
-  },
-  pause: function() {
-    this.video.pause();
+      if (cursorPos > screenCenter) {
+        side = "right";
+      } else {
+        side = "left";
+      }
+
+      return side;
+    };
+
+    this.setCursorEvent = function() {
+      presentation.media.addEventListener("mousemove", ev => {
+        let side = this.checkCursorSide(ev.screenX);
+        this.alignSubs(side);
+      });
+    };
+
+    this.init = function() {
+      this.setCursorEvent();
+    };
+  }
+}
+
+class Presentation {
+  constructor(element) {
+    this.wrapper = element;
+    this.media = this.wrapper.querySelector(".media");
+    this.video = this.wrapper.querySelector(".media video");
+    this.slideEl = this.wrapper.querySelector(".media .current-slide");
+    this.slidesEl = this.wrapper.querySelector(".slides");
+    this.seekerEl = this.wrapper.querySelector(".seeker");
+    this.speedUpEl = this.wrapper.querySelector(".speed-up");
+    this.speedDownEl = this.wrapper.querySelector(".speed-down");
+    this.speedDropdown = this.wrapper.querySelector("#set-speed");
+    this.currentSlideEl = null;
     this.playing = false;
-    this.media.classList.add("paused");
-    clearInterval(this.interval);
-  },
-  playPause: function() {
-    if (!this.playing) {
-      this.play();
-    } else {
-      this.pause();
-    }
-  },
-  speedUp: function() {
-    this.video.playbackRate += 0.2;
-    console.log(this.video.playbackRate);
-  },
-  speedDown: function() {
-    this.video.playbackRate -= 0.2;
-    console.log(this.video.playbackRate);
-  },
-  setSpeed: function(speed) {
-    this.video.playbackRate = speed;
-    console.log(this.video.playbackRate);
-  },
-  addEvents: function() {
-    this.media.addEventListener("click", ev => {
-      this.playPause();
-    });
+    this.slideNodes = [];
+    this.videoDuration = 0;
+    this.interval = null;
 
-    if (this.speedDropdown) {
-      this.speedDropdown.addEventListener("change", ev => {
-        this.setSpeed(Number(ev.target.value));
-      });
-    }
+    this.createSlides = function() {
+      slides.forEach(slide => {
+        let img = document.createElement("img");
+        img.src = slide.img;
 
-    if (this.speedDownEl) {
-      this.speedDownEl.addEventListener("click", ev => {
-        this.speedDown();
-      });
-    }
+        img.addEventListener("click", ev => {
+          this.changeTime(slide.startTime);
+          this.updatePresentation();
+        });
 
-    if (this.speedUpEl) {
-      this.speedUpEl.addEventListener("click", ev => {
-        this.speedUp();
+        this.slideNodes.push(img);
+        this.slidesEl.insertAdjacentElement("beforeend", img);
       });
-    }
-  },
-  init: function() {
-    this.createSlides();
-    this.setSeeker();
-    this.handleSeeker();
-    this.updatePresentation();
-    this.addEvents();
+    };
+
+    this.getSlide = function(currentTime) {
+      let currentSlide = null;
+
+      slides.filter(function(slide) {
+        if (currentTime >= slide.startTime && currentTime <= slide.endTime) {
+          currentSlide = slide;
+        }
+      });
+
+      return currentSlide;
+    };
+
+    this.setActiveSlide = function(slide) {
+      this.slideNodes.forEach(function(slide) {
+        slide.classList.remove("active");
+      });
+
+      this.currentSlideEl = this.slideNodes[slide.index];
+      this.currentSlideEl.classList.add("active");
+    };
+
+    this.setSlide = function() {
+      let slide = this.getSlide(this.video.currentTime);
+      if (slide) {
+        this.slideEl.src = slide.img;
+        this.setActiveSlide(slide);
+      }
+    };
+
+    this.changeTime = function(time) {
+      this.video.currentTime = time;
+    };
+
+    this.setSeeker = function() {
+      this.video.addEventListener("durationchange", ev => {
+        this.duration = this.video.duration;
+        this.seekerEl.max = this.duration;
+      });
+    };
+
+    this.updateSeeker = function() {
+      this.seekerEl.value = this.video.currentTime;
+    };
+
+    this.handleSeeker = function() {
+      const _this = this;
+      this.seekerEl.addEventListener("change", function() {
+        _this.changeTime(this.value);
+        _this.updatePresentation();
+      });
+    };
+
+    this.startInterval = function() {
+      const _this = this;
+      this.interval = setInterval(function() {
+        _this.updatePresentation();
+      }, config.intervalTime);
+    };
+
+    this.updatePresentation = function() {
+      this.setSlide();
+      this.updateSeeker();
+      subtitles.setSubtitle();
+
+      if (this.video.currentTime === this.duration) {
+        this.pause();
+      }
+    };
+
+    this.play = function() {
+      location.href = "#presentation";
+      this.video.play();
+      this.playing = true;
+      this.media.classList.remove("paused");
+      this.startInterval();
+    };
+
+    this.pause = function() {
+      this.video.pause();
+      this.playing = false;
+      this.media.classList.add("paused");
+      clearInterval(this.interval);
+    };
+
+    this.playPause = function() {
+      if (!this.playing) {
+        this.play();
+      } else {
+        this.pause();
+      }
+    };
+
+    this.speedUp = function() {
+      this.video.playbackRate += 0.2;
+      console.log(this.video.playbackRate);
+    };
+
+    this.speedDown = function() {
+      this.video.playbackRate -= 0.2;
+      console.log(this.video.playbackRate);
+    };
+
+    this.setSpeed = function(speed) {
+      this.video.playbackRate = speed;
+      console.log(this.video.playbackRate);
+    };
+
+    this.addEvents = function() {
+      this.media.addEventListener("click", ev => {
+        this.playPause();
+      });
+
+      if (this.speedDropdown) {
+        this.speedDropdown.addEventListener("change", ev => {
+          this.setSpeed(Number(ev.target.value));
+        });
+      }
+
+      if (this.speedDownEl) {
+        this.speedDownEl.addEventListener("click", ev => {
+          this.speedDown();
+        });
+      }
+
+      if (this.speedUpEl) {
+        this.speedUpEl.addEventListener("click", ev => {
+          this.speedUp();
+        });
+      }
+    };
+
+    this.init = function() {
+      this.createSlides();
+      this.setSeeker();
+      this.handleSeeker();
+      this.updatePresentation();
+      this.addEvents();
+    };
   }
-};
+}
+
+const subtitleElement = document.querySelector(".subtitle");
+const subtitles = new Subtitles(subtitleElement, subs);
+
+const presentationElement = document.querySelector(".presentation");
+const presentation = new Presentation(presentationElement);
 
 if (document.querySelector(".presentation")) {
   presentation.init();
