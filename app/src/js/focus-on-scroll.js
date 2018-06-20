aae5972a69ce6baac697fabe5a6f2ee8f442bcb2
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-06-20 16:38:42 +0200

    Swap code

--- app/src/js/focus-on-scroll.js
+++ app/src/js/focus-on-scroll.js
@@ -1,58 +1,18 @@
-const allArticles = document.querySelectorAll('.focusOnScroll');
+const allFocusableElements = document.querySelectorAll(
+  'a, input, select, textarea, button'
+);
+const backToNavigationLinks = document.querySelectorAll('.back-to-navigation');
+const firstNavItem = document.getElementById('first-navigation-item');
 
-let pauseFocusOnScroll = false;
-
-function isScrolledIntoView(element) {
-  const elemTop = element.getBoundingClientRect().top;
-  const elemBot = element.getBoundingClientRect().bottom;
-  const elemMid = (elemTop + elemBot) / 2;
-  const offset = (window.innerHeight / 100) * 10;
-
-  return (
-    elemMid >= window.innerHeight / 2 - offset &&
-    elemMid <= window.innerHeight / 2 + offset
-  );
-}
-
-function setFocusState(article) {
-  if (article.querySelector('a, input, select, textarea, button')) {
-    const articleFirstLink = article.querySelector(
-      'a, input, select, textarea, button'
-    );
-    const allArticleLinks = article.querySelectorAll(
-      'a, input, select, textarea, button'
-    );
-    for (let i = 0; i < allArticleLinks.length; i++) {
-      if (document.activeElement === allArticleLinks[i]) {
-        return false;
-      }
-    }
-    console.log(articleFirstLink, 'Focused');
-    articleFirstLink.focus();
-  }
-}
-
-function checkAllArticles() {
-  allArticles.forEach(article => {
-    if (isScrolledIntoView(article)) {
-      setFocusState(article);
-    }
+backToNavigationLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    firstNavItem.focus();
   });
-}
-
-window.addEventListener('scroll', () => {
-  if (!pauseFocusOnScroll) {
-    checkAllArticles();
-  }
 });
 
-window.addEventListener('keydown', e => {
-  if (e.keyCode === 32 || e.keyCode === 38 || e.keyCode === 40) {
-    checkAllArticles();
-  } else {
-    pauseFocusOnScroll = true;
-    setTimeout(() => {
-      pauseFocusOnScroll = false;
-    }, 1000);
-  }
+allFocusableElements.forEach(element => {
+  element.addEventListener('mouseover', () => {
+    element.focus();
+  });
 });
