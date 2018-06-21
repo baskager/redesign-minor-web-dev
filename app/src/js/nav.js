8002a68256ef91a4c9056b01d6d888c979ed8dd5
Jamal van Rooijen <jamal@vergelijkgroep.nl>
2018-06-21 16:07:44 +0200

    Code stylo is sehr netjes

--- app/src/js/nav.js
+++ app/src/js/nav.js
@@ -9,27 +9,30 @@
     keyNavState: true,
 
     init: function() {
-      let self = this;
-      // Check the state of the hotkeys that is stored in localstorage.
-      if (!storage.get("hotKeysEnabled")) {
-        storage.store("hotKeysEnabled", this.keyNavState);
-      } else {
-        this.keyNavState = storage.get("hotKeysEnabled");
-      }
-
+      keyNavToggle.toggleState();
+      keyNavToggle.toggleIndicators();
       eventListeners.onKeyUp();
       eventListeners.onKeyDown();
+    }
+  };
 
-      // Set base state for the toggle indicator
-      self.toggleIndicators();
+  const keyNavToggle = {
+    toggleState: function(e) {
+      // Check the state of the hotkeys that is stored in localstorage.
+      if (!storage.get("hotKeysEnabled")) {
+        storage.store("hotKeysEnabled", keyNav.keyNavState);
+      } else {
+        keyNav.keyNavState = storage.get("hotKeysEnabled");
+      }
     },
 
-    toggleIndicators: function() {
+    toggleIndicators: function(e) {
+      // let self = this;
       let mainIndicator = document.getElementById("keynav");
       let menuIndicators = document.querySelectorAll(
         ".main-nav div > ul a span"
       );
-      if (this.keyNavState) {
+      if (keyNav.keyNavState) {
         mainIndicator.classList.add("active");
         menuIndicators.forEach(function(indicator) {
           indicator.classList.add("active");
@@ -59,7 +62,7 @@
 
   // Set buttons for the navigation with keys
   const keyNavKeys = {
-    init: function keyNavKeys(e) {
+    keyNavKeys: function(e) {
       var paths = {
         49: "/",
         50: "/program",
@@ -75,57 +78,26 @@
     }
   };
 
-  // Add functionality to the keys
-  const toggleOnInput = {
-    // Get all rectangles with numbers next to the menu items
-    getSpan: document.querySelectorAll(".main-nav div > ul a span"),
-
-    disableOnInput: function() {
-      // Disable keyCode navigation when an input field has focus
-      getInput.forEach(function(input) {
-        input.addEventListener("focus", function(input) {
-          console.log("hai");
-          keyNavState = false;
-
-          // Add 'inactive' styling to the rectangles
-          getSpan.forEach(function(span) {
-            span.classList.add("inactive");
-          });
-        });
-      });
-    },
-
-    enableOnInput: function() {
-      // Enable keyCode navigation when input field loses focus
-      getInput.forEach(function(el) {
-        keyNavState = true;
-
-        // Remove 'inactive' styling on the rectangles
-        getSpan.forEach(function(span) {
-          span.removeAttribute("class");
-        });
-      });
-    }
-  };
-
   const eventListeners = {
     onKeyDown: function(e) {
       window.addEventListener("keydown", function(e) {
+        console.log("hai");
         // checken op metakey
         if (e.keyCode === 91) {
-          self.cmdState = true;
+          keyNav.cmdState = true;
         }
 
         if ((e.altKey || e.keyCode === 18) && (e.ctrlKey || e.keyCode === 17)) {
           // Set the opposite of what the current keyNavState is
-          self.keyNavState = !self.keyNavState;
-          storage.store("hotKeysEnabled", self.keyNavState);
+          keyNav.keyNavState = !keyNav.keyNavState;
+          storage.store("hotKeysEnabled", keyNav.keyNavState);
 
           // Retoggle the state of the indicator
-          self.toggleIndicators();
+          keyNavToggle.toggleState();
+          keyNavToggle.toggleIndicators();
         }
 
-        keyNavKeys.init(e);
+        keyNavKeys.keyNavKeys(e);
       });
     },
 
@@ -137,5 +109,39 @@
       });
     }
   };
+
+  // // Add functionality to the keys
+  // const toggleOnInput = {
+  //   // Get all rectangles with numbers next to the menu items
+  //   getSpan: document.querySelectorAll(".main-nav div > ul a span"),
+
+  //   disableOnInput: function () {
+  //     // Disable keyCode navigation when an input field has focus
+  //     getInput.forEach(function (input) {
+  //       input.addEventListener("focus", function (input) {
+  //         console.log("hai");
+  //         keyNavState = false;
+
+  //         // Add 'inactive' styling to the rectangles
+  //         getSpan.forEach(function (span) {
+  //           span.classList.add("inactive");
+  //         });
+  //       });
+  //     });
+  //   },
+
+  //   enableOnInput: function () {
+  //     // Enable keyCode navigation when input field loses focus
+  //     getInput.forEach(function (el) {
+  //       keyNavState = true;
+
+  //       // Remove 'inactive' styling on the rectangles
+  //       getSpan.forEach(function (span) {
+  //         span.removeAttribute("class");
+  //       });
+  //     });
+  //   }
+  // };
+
   keyNav.init();
 })();
