dcdfc2b254e1f854b2a0179432fc67bc060739ea
Jelle Overbeek <j.overbeek@recognize.nl>
2018-06-21 14:52:34 +0200

    data is now injected directly from the json files and now not loaded into the memory anymore

--- app/server.js
+++ app/server.js
@@ -9,24 +9,11 @@ function readData(path) {
   return JSON.parse(data);
 }
 
-let homepageData = readData("src/json/homepage.json");
-let partnersData = readData("src/json/partners.json");
-let teamData = readData("src/json/team.json");
-let programData = readData("src/json/program.json");
-let studentworkData = readData("src/json/student-work.json");
-let talkData = readData("src/json/talk.json");
-let signupData = readData("src/json/signup.json");
-let contactData = readData("src/json/contact.json");=
-
 function getSubs(path) {
   const srt = fs.readFileSync(path, "utf8");
   const subs = parser.fromSrt(srt, true);
   return subs;
 }
-
-talkData.subtitles = getSubs("src/subs/talk.srt");
-talkData.subtitles = JSON.stringify(talkData.subtitles);
-talkData.slides = JSON.stringify(talkData.slides);
 
 nunjucks.configure("./templates", {
   autoescape: true,
@@ -39,39 +26,44 @@ app.use(express.static("./static"));
 
 app.get("/", function(req, res) {
   res.render("index.html", {
-    data: homepageData
+    data: readData("src/json/homepage.json")
   });
 });
 
 app.get("/course", function(req, res) {
   res.render("course.html", {
-    data: courseData
+    data: readData("src/json/course.json")
   });
 });
 
 app.get("/program", function(req, res) {
   res.render("program.html", {
-    data: programData
+    data: readData("src/json/program.json")
   });
 });
 
 app.get("/partners", function(req, res) {
   res.render("partners.html", {
-    data: partnersData
+    data: readData("src/json/partners.json")
   });
 });
 
 app.get("/team", function(req, res) {
   res.render("partners-team.html", {
-    data: teamData
+    data: readData("src/json/team.json")
   });
 });
 
 app.get("/student-work", function(req, res) {
   res.render("student-work.html", {
-    data: studentworkData
+    data: readData("src/json/student-work.json")
   });
 });
+
+let talkData = readData("src/json/weekly-nerd.json");
+talkData.subtitles = getSubs("src/subs/talk.srt");
+talkData.subtitles = JSON.stringify(talkData.subtitles);
+talkData.slides = JSON.stringify(talkData.slides);
 
 app.get("/weekly-nerd", function(req, res) {
   res.render("talk.html", {
@@ -81,13 +73,13 @@ app.get("/weekly-nerd", function(req, res) {
 
 app.get("/contact", function(req, res) {
   res.render("contact.html", {
-    data: contactData
+    data: readData("src/json/contact.json")
   });
 });
 
 app.get("/signup", function(req, res) {
   res.render("signup.html", {
-    data: signupData
+    data: readData("src/json/signup.json")
   });
 });
 
