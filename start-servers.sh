5486f32670335ad01fa8f9c55e2a4c92d19877c0
Bas Kager <baskager@hotmail.com>
2018-06-25 16:04:29 +0200

    Docker: Make database connection work between containers

--- start-servers.sh
+++ start-servers.sh
@@ -1,6 +1,6 @@
 if [[ $NODE_ENV == "development" ]]; then 
     echo "Composing containers for development"
-    docker-compose -f docker-compose.dev.yml up  --build -d
+    docker-compose -f docker-compose.dev.yml up --build -d
 else
     echo "Composing containers for production".
     docker-compose -f docker-compose.prod.yml up --build -d
