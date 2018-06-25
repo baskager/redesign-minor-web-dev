dde59cdab8af39dbf0ec26792f94678719816fbe
Bas Kager <baskager@hotmail.com>
2018-06-25 11:56:11 +0200

    Database: updated course_composed_view for the program timeline

--- sql/course_composed_view.sql
+++ sql/course_composed_view.sql
@@ -3,8 +3,10 @@ SELECT
     CONCAT(course_week.course_id, course_week.week_number) AS course_week_pk,
     course.id AS course_id,
     course.name AS course_name,
-    teacher.id AS teacher_id,
     course_week.week_number AS week_number,
+    course_period.number AS course_period_number,
+    course_period.weeks AS course_period_weeks,
+    teacher.id AS teacher_id,
     teacher.name AS teacher_name,
     teacher.avatar_url AS teacher_avatar,
     teacher.short_story AS teacher_short_story,
@@ -30,6 +32,8 @@ LEFT JOIN partner
     ON partner.id = course.partner_id
 INNER JOIN course_type
     ON course.type_id = course_type.id
+INNER JOIN course_period
+    ON course.period_id = course_period.number
 RIGHT JOIN course_week
     ON course.id = course_week.course_id
 WHERE course.active = TRUE
