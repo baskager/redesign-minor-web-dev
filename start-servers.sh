380203045e98c3a365a9b2c9c51e1808a54b0f73
Bas Kager <baskager@hotmail.com>
2018-06-25 14:37:32 +0200

    Docker: rebuild 'web' image on each start

--- start-servers.sh
+++ start-servers.sh
@@ -1,9 +1,7 @@
 if [[ $NODE_ENV == "development" ]]; then 
     echo "Composing containers for development"
-    docker build .
-    docker-compose -f docker-compose.dev.yml up -d
+    docker-compose -f docker-compose.dev.yml up  --build -d
 else
-    echo "Composing containers for production"
-    docker build .
-    docker-compose -f docker-compose.prod.yml up -d
+    echo "Composing containers for production".
+    docker-compose -f docker-compose.prod.yml up --build -d
 fi
