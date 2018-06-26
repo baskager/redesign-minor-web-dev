fa97caad5a72c1c8cfc26c28a211c8ab161d7691
Bas Kager <baskager@hotmail.com>
2018-06-26 17:29:35 +0200

    Database: New data structure and better content for courses

--- sql/dump/minorwebdev.sql
+++ sql/dump/minorwebdev.sql
@@ -48,7 +48,8 @@ CREATE TABLE public.course (
     page_slug character varying NOT NULL,
     long_description text,
     learning_goals jsonb,
-    icon_url character varying(512)
+    icon_url character varying(512),
+    color character varying(7)
 );
 
 
@@ -169,6 +170,7 @@ CREATE VIEW public.course_composed_view AS
     course.short_description AS course_description,
     course_type.name AS course_type,
     course.page_slug,
+    course.color,
     course.icon_url,
     course.long_description,
     course.learning_goals,
@@ -424,12 +426,16 @@ ALTER TABLE ONLY public.testimonial ALTER COLUMN id SET DEFAULT nextval('public.
 -- Data for Name: course; Type: TABLE DATA; Schema: public; Owner: minorwebdev
 --
 
-COPY public.course (id, name, short_description, partner_id, type_id, active, period_id, page_slug, long_description, learning_goals, icon_url) FROM stdin;
-4	Web Design	Creating websites is getting more and more complex and is usually not a one person job. It is important to ensure that design is consistent and optimized to meet business objectives and create enjoyable experiences for users.	\N	3	t	3	web-design	The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.	["The river stole the gods.", "Don't step on the broken glass.", "If Purple People Eaters are real… where do they find purple people to eat?"]	https://pre00.deviantart.net/25c6/th/pre/f/2016/117/0/4/bridge___flat_design_wallpaper_by_sebastian456-da0elq8.png
-7	CSS To The Rescue	CSS & accessibility	\N	3	t	1	css-to-the-rescue	\N	\N	https://assets.wordpress.envato-static.com/uploads/2016/03/Tools-for-Structuring-Optimizing-Your-CSS-Code.jpg
-8	Web app from scratch	Javascript introduction	\N	3	t	1	web-app-from-scratch	\N	\N	https://cdn-images-1.medium.com/max/1600/1*uiYye9J142y8fIZGRnufEw.png
-5	Project 1	At the OBA we will create awesome stuff with data about Amsterdam, which they collected throughout the years.	1	1	t	1	project-1	\N	\N	\N
-3	Real Time Web	In drie weken leer je hoe je real time / live data op een inzichtelijke manier toegankelijk kunt maken voor de eindgebruiker.	\N	3	t	3	real-time-web	During this course you'll learn to use Socket.io to establish a connection between the browser and the server. You'll combine your knowledge from other courses and your newly aqquired skills from this course to create a progressive web app that retrieves live data from an API, and display the data to the user.	["Student is able to build a (Node) webapp with a back-end that uses templating, routing and a remote database.", "Student can deliver a live application where the interaction influences the content of the application by manipulating the database.", "Student can translate live data to a reactive view (datavisualisation is allowed) that is useful for the end user."]	/img/partners/oba.jpg
+COPY public.course (id, name, short_description, partner_id, type_id, active, period_id, page_slug, long_description, learning_goals, icon_url, color) FROM stdin;
+7	CSS To The Rescue	CSS & accessibility	\N	3	t	1	css-to-the-rescue	\N	\N	https://assets.wordpress.envato-static.com/uploads/2016/03/Tools-for-Structuring-Optimizing-Your-CSS-Code.jpg	#4e00af
+4	Web Design	Design like a pro	\N	3	t	3	web-design	The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.	["The river stole the gods.", "Don't step on the broken glass.", "If Purple People Eaters are real… where do they find purple people to eat?"]	https://pre00.deviantart.net/25c6/th/pre/f/2016/117/0/4/bridge___flat_design_wallpaper_by_sebastian456-da0elq8.png	#C6006B
+9	Performance matters	Website performance	\N	3	t	2	performance-matters	This should be a long description	\N	https://i.pinimg.com/originals/6f/8f/a0/6f8fa03e7e73635f8ba88e0aabfd49bd.jpg	#007a4e
+8	Web app from scratch	Javascript introduction	\N	3	t	1	web-app-from-scratch	\N	\N	https://cdn-images-1.medium.com/max/1600/1*uiYye9J142y8fIZGRnufEw.png	#005aad
+10	Browser Technologies	Progressive Enhancement	\N	3	t	2	browser-technologies	A long description still needs to be added	\N	http://blog.teamtreehouse.com/wp-content/uploads/2014/11/progressive-enhancement.png	#233e68
+5	Project 1	At the OBA we will create awesome stuff with data about Amsterdam which they collected throughout the years.	1	1	t	1	project-1	\N	\N	\N	\N
+11	Project 2	Lifely is a digital agency. Here we’re going to work in their codebase of multiple projects.	2	1	t	2	project-2	\N	\N	\N	\N
+12	Project 3	De Ceuvel is a playground for green innovation. Here we will see what we can do with green technology	3	1	t	3	project-3	\N	\N	http://deceuvel.nl/wp-content/uploads/2016/06/De-Ceuvel1.jpg	\N
+3	Real Time Web	Live data	\N	3	t	3	real-time-web	During this course you'll learn to use Socket.io to establish a connection between the browser and the server. You'll combine your knowledge from other courses and your newly aqquired skills from this course to create a progressive web app that retrieves live data from an API, and display the data to the user.	["Student is able to build a (Node) webapp with a back-end that uses templating, routing and a remote database.", "Student can deliver a live application where the interaction influences the content of the application by manipulating the database.", "Student can translate live data to a reactive view (datavisualisation is allowed) that is useful for the end user."]	/img/partners/oba.jpg	#901265
 \.
 
 
@@ -440,6 +446,7 @@ COPY public.course (id, name, short_description, partner_id, type_id, active, pe
 COPY public.course_period (number, weeks) FROM stdin;
 1	[1, 2, 3, 4]
 3	[9, 10, 11, 12]
+2	[5, 6, 7, 8]
 \.
 
 
@@ -459,9 +466,6 @@ COPY public.course_type (id, name) FROM stdin;
 --
 
 COPY public.course_week (course_id, week_number, description, subjects_covered) FROM stdin;
-4	1	CSS startup	["The river stole the gods.", "Don't step on the broken glass.", "If Purple People Eaters are real… where do they find purple people to eat?"]
-4	2	Just use weird selectors	["Different data", "Don't step on the broken glass.", "Allways be weary near Ronald McDonald"]
-4	3	Time to finish everything up :)	["Different data", "Don't step on the broken glass.", "Allways be weary near Ronald McDonald"]
 7	1	Creating fun, pleasurable and accessible solutions for different kinds of interfaces.	[]
 7	2	Using CSS without the use of classes. Getting to know the basic CSS principles is key!	[]
 8	1	Introduction to Javascript object programming and coding design patterns.	[]
@@ -469,6 +473,20 @@ COPY public.course_week (course_id, week_number, description, subjects_covered) 
 8	3	Designing your pleasurable interface with attention to detail.	[]
 7	3	Finetune the structure and flow of your app, because It's a wrap!	[]
 5	1	\N	\N
+9	1	Make the basic functionality work on on as many devices as possible.	[]
+9	2	Add feature detection to one of your applications.	\N
+9	3	Turn your application into a three layer progressive enhancent one.	[]
+10	2	Add serverside rendering to one of your applications, also turn it in to a progressive web app.	[]
+10	3	Add more optimalisations and a service worker. Make your application work offline.	[]
+11	1	\N	\N
+3	1	Set up your very own development environment and build/deploy a basic app	[]
+3	2	Learn about client-server communication and connect with a real-time data source	[]
+3	3	Continue working on your own API and learn about different ways to share your data	[]
+12	1	\N	\N
+4	1	Design a user interface for a use case. Make your design in HTML, CSS and Javascript keeping at least three interface design principles in mind	["The river stole the gods.", "Don't step on the broken glass.", "If Purple People Eaters are real… where do they find purple people to eat?"]
+4	2	User needs and usability. Write user stories and test with real people.	["Different data", "Don't step on the broken glass.", "Allways be weary near Ronald McDonald"]
+4	3	Learn how to implement feedback in your design. Loading web pages, showing error messages and much more 	["Different data", "Don't step on the broken glass.", "Allways be weary near Ronald McDonald"]
+10	1	Do a performance audit of an existing site and make a plan to improve it.	[]
 \.
 
 
@@ -478,6 +496,8 @@ COPY public.course_week (course_id, week_number, description, subjects_covered) 
 
 COPY public.partner (id, name, logo_url, website_url) FROM stdin;
 1	Public Library of Amsterdam	http://www.thonik.nl/app/uploads/07-thonik-amsterdam-openbare_bibliotheek_amsterdam_OBA-library-logo_uitleg-filmstill-1440x810.jpg	https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwjb3InB48vbAhVCVxQKHTsdATYQFggpMAA&url=https%3A%2F%2Fwww.oba.nl%2F&usg=AOvVaw06a4oMxeuReoEHbO8YMRqX
+2	Lifely	https://pbs.twimg.com/profile_images/803168896157159426/QphLOW3s_400x400.jpg	http://www.lifely.nl
+3	De Ceuvel	https://deceuvel.nl/wp-content/themes/ceuvel/images/logo.svg	https://deceuvel.nl
 \.
 
 
@@ -497,6 +517,7 @@ COPY public.teacher (id, type, name, repository_url, website_url, avatar_url, sh
 2	1	Koop Reynders	https://github.com/KoopReynders	\N	https://avatars2.githubusercontent.com/u/1391509?s=460&v=4	Masta stylo
 1	1	Vasilis van Gemert	https://github.com/vasilisvg	http://vasilis.nl/	https://a11yrules.com/wp-content/uploads/2018/01/Vasilis-van-Gemert.jpg	Hallo
 3	1	Joost Faber	\N	\N	https://media.licdn.com/dms/image/C4D03AQGjbmp7vQhU6Q/profile-displayphoto-shrink_200_200/0?e=1534982400&v=beta&t=z9pL5r8zgBjYGuQE-56NCJzMYVggUYxIizvpxVotNj4	
+7	1	Krijn Hoetmer	\N	https://krijnhoetmer.nl	https://krijnhoetmer.nl/als-plaatje.jpg	Freelance web developer
 \.
 
 
@@ -511,6 +532,10 @@ COPY public.teacher_course (teacher_id, course_id) FROM stdin;
 1	7
 3	8
 1	5
+7	10
+3	9
+1	11
+1	12
 \.
 
 
@@ -536,7 +561,7 @@ COPY public.testimonial (id, name, role, message) FROM stdin;
 -- Name: course_id_seq; Type: SEQUENCE SET; Schema: public; Owner: minorwebdev
 --
 
-SELECT pg_catalog.setval('public.course_id_seq', 8, true);
+SELECT pg_catalog.setval('public.course_id_seq', 12, true);
 
 
 --
@@ -550,7 +575,7 @@ SELECT pg_catalog.setval('public.course_type_id_seq', 3, true);
 -- Name: partner_id_seq; Type: SEQUENCE SET; Schema: public; Owner: minorwebdev
 --
 
-SELECT pg_catalog.setval('public.partner_id_seq', 1, true);
+SELECT pg_catalog.setval('public.partner_id_seq', 3, true);
 
 
 --
@@ -564,7 +589,7 @@ SELECT pg_catalog.setval('public.student_work_id_seq', 1, false);
 -- Name: teacher_teacher_id_seq; Type: SEQUENCE SET; Schema: public; Owner: minorwebdev
 --
 
-SELECT pg_catalog.setval('public.teacher_teacher_id_seq', 6, true);
+SELECT pg_catalog.setval('public.teacher_teacher_id_seq', 7, true);
 
 
 --
